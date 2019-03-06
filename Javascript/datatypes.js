55478d8436ba9c2f0e1967099c7469115dd63781
Iris Zou <zouqiyue_iris@outlook.com>
2019-03-05 22:17:42 -0500

    Reviewed Javascript Basic

---
+++ Javascript/datatypes.js
@@ -0,0 +1,49 @@
+//5 datatypes in javascript
+
+//Numbers
+4
+9.3
+-10
+
+//Strings
+"Hello World"
+"43"
+
+//Booleans
+true
+false
+
+//null and undefined
+null
+undefined
+
+//variables 
+
+var yourVariableName = yourValue;
+var name="Rusty";
+var secretNumeber=73;
+var isAdorable =true;
+
+var name = "Rusty";
+"hello there "+name
+
+var num = 37;
+num+3+10
+
+var name = "Robert";
+name = "Bob";
+
+//Null and undefined
+var name;
+var age;
+
+var currentPlayer = "charlie";
+currentPlayer = null; //game over
+
+
+//3 built in function
+
+//alert
+//prompt
+//console.log
+
---
+++ Javascript/script.js
@@ -0,0 +1,13 @@
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+// var age = prompt("What is your age?");
+
+// console.log("Your full name is "+firstName+" "+lastName);
+// console.log("Your are "+age+" year old");
+
+console.log("Age Calculator")
+
+var age = prompt("How old are you?");
+var days = age*365;
+
+alert(age+" is roughly "+days+" days");

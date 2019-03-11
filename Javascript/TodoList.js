615f71c687ef7d376988adbd204e6626bad6b8f8
Iris Zou <zouqiyue_iris@outlook.com>
2019-03-10 23:21:23 -0400

    Javascript array reviewed done

--- Javascript/TodoList.js
+++ Javascript/TodoList.js
@@ -1,17 +1,20 @@
 var todos=[]
 
-
-
-
-
 function todolist(){
 	var input=prompt("What would you like to do");
 	while (input!=="quit"){
 		if (input==="list"){
+			listTodos()
+			
 		console.log(todos);
 		} else if(input==="new"){
 			var newTodo=prompt("Enter new todo");
 			todos.push(newTodo);
+			console.log("Added Todo");
+		}else if (input==="delete") {
+			var index=prompt("Enter index of the todo to delete");
+			todos.splice(index,1);
+			console.log("Deleted Todo");
 		}
 		input=prompt("What would you like to do");
 	}
@@ -21,5 +24,14 @@ window.setTimeout(function(){todolist()
 
 }, 1000);
 
+function listTodo(){
+	console.log("*******");
+	todos.forEach(function(todo,i){
+
+		console.log(i+": ",todo);
+	});
+	console.log("*******");
+
+}
 
 console.log("Ok, you quit the app");
--- Javascript/array.js
+++ Javascript/array.js
@@ -28,8 +28,78 @@ friends.indexOf();
 // non parameter means the all array
 friends.slice(start index, end index)
 
+//delete element by index
+friends.splice(index,number)
+
 
 friends.forEach(function(fri){
 	console.log(fri);
 })
 
+
+function printReverse(array){
+	// while (array.length!=0){
+	// 	console.log(array.pop());
+
+	// }
+	for (var i = array.length-1; i >=0; i--) {
+		console.log(array[i]);
+	}
+}
+
+printReverse(friends);
+
+
+function isUniform(arr){
+	var first=arr[0];
+	arr.forEach(function(a){
+		if(a!=first){
+			return false;
+		}
+	});
+	return true;
+}
+
+isUniform(friends);
+
+function max(arr){
+	var max=arr[0];
+	arr.forEach(function (a) {
+		if(a>max){
+			max=a;
+		}
+	});
+	console.log(max);
+
+}
+
+max(friends);
+
+function sumArray(array){
+	var s=0;
+	array.ForEach(function(a){
+		s+=a;
+	});
+	console.log(s);
+}
+
+sumArray(friends);
+
+
+function myForEach(arr,func){
+	//loop through array
+	for(var i=0; i<arr.length; i++){
+		//call func for each item in array
+		func(arr[i]);
+	}
+
+}
+
+
+Array.prototype.myForEach=function(func){
+	for (var i=0;i<this.length;i++){
+		func(this[i]);
+	}
+}
+
+friends.myForEach(alert)

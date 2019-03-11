5b2e09f8399a83135f12e37f67513cd93138fbe9
Iris Zou <zouqiyue_iris@outlook.com>
2019-03-10 23:58:54 -0400

    Javascript Object review done

---
+++ Javascript/object.js
@@ -0,0 +1,32 @@
+//object does not have any built-in function
+
+//example
+
+var person={
+	name: "Iris",
+	age:22,
+	city:"Ithaca"
+}
+
+//person.name or person["age"]
+
+var movies=[{
+	name:"In Bruges",
+	evaluate:"5 starts",
+	seen:"yes"
+},
+{
+	name:"Frozen",
+	evaluate:"4.5 starts",
+	seen:"no"
+},
+{
+	name:"Mad Max Fury Road",
+	evaluate:"5 starts",
+	seen:"yes"
+},
+{
+	name:"Les Miserables",
+	evaluate:"3.5 starts",
+	seen:"no"
+}]

5c373235706cd223d957d4309e04ddf715f29082
Iris Zou <zouqiyue_iris@outlook.com>
2019-03-11 16:31:13 -0400

    jquery basic reviewed

---
+++ Javascript/jqr.js
@@ -0,0 +1,27 @@
+//select
+
+$("img");
+
+$(".sale");
+
+$("#bonus");
+
+$("li a");
+
+
+$("ul li").css("color","yellow");
+
+$("ul li").css({
+	fontSize:"10px",
+	border:"3px dashed purple"
+
+}); 
+
+//exercise
+$("div").css("color","purple");
+
+$("div.highlight").css("width","200px");
+
+$("dix#third").css("border-color","orange");
+
+$("div:first-of-type").css("color","pink");

8d8e80c673897e39e55a2270bf64dd5dffd17be4
Iris Zou <zouqiyue_iris@outlook.com>
2019-03-12 11:07:13 -0400

    advanced jquery reviewed

--- Javascript/jqr.js
+++ Javascript/jqr.js
@@ -25,3 +25,37 @@ $("div.highlight").css("width","200px");
 $("dix#third").css("border-color","orange");
 
 $("div:first-of-type").css("color","pink");
+
+.val()//extract value from input
+.text()
+.attr()//get the value of an attribute for the first element 
+.html()
+.last()
+
+$("img").attr(attributename,value);
+
+.addClass()
+.removeClass()
+.toggleClass()
+
+//event
+$("item").click(function(){
+	$(this).css("background","pink");
+});
+
+.keydown()//indicate which key is pressed
+
+.keyup()
+.keypress()//indicate which character is pressed
+
+$("input").keypress(function(event){
+	if(event.which===13){
+		//13 is enter
+		console.log();
+	}
+});
+
+$("input").on()
+
+//click() only effect on existed element
+//on("click") can effect on potential future element
